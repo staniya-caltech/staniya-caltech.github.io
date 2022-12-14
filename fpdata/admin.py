f88f5d093c334913450d086d4597291953f7842e
staniya <staniya@g.hmc.edu>
2022-12-14 15:59:10 -0800

    Update: Configuring working file structure

--- fpdata/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ZTFFPSData, MROZData, AndrewData
-
-admin.site.register(ZTFFPSData)
-admin.site.register(MROZData)
-admin.site.register(AndrewData)
